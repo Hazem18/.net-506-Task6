e090752c20986bbecdd421fc41a71a5d8d68272c
Hazem18 <139848488+Hazem18@users.noreply.github.com>
2024-08-07 21:08:14 +0300

    Add files via upload

---
+++ GenericTask/GenericTask/ClsCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTask
+{
+    internal class ClsCalculator
+    {
+        static public bool AreEqual<T>(T x , T y)
+        {
+            return EqualityComparer<T>.Default.Equals(x, y); 
+        }
+    }
+}
---
+++ GenericTask/GenericTask/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //bool IsEqual = ClsCalculator.AreEqual<int>(10, 20);
+            //bool IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+            bool IsEqual = ClsCalculator.AreEqual<double>(10.5, 20.5);
+            if (IsEqual)
+            {
+                Console.WriteLine("Both are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both are Not Equal");
+            }
+        }
+    }
+}
